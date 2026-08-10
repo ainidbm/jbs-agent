82202966e13b689b51efca6ee71fe71d70e0a967
胡一鸣 <16859723+hu-manmao@user.noreply.gitee.com>
2026-08-10 16:47:37 +0800

    Fix: HashRouter for SPA + base path /jbs-agent/

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { ConfigProvider, theme } from "antd";
 import zhCN from "antd/locale/zh_CN";
 import App from "./App";
@@ -19,9 +19,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         },
       }}
     >
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </ConfigProvider>
   </React.StrictMode>
 );
